e363feb83e9442996f51cd2a3c38ef38930889b8
Sameer <sameer.thapa2012@gmail.com>
2016-11-25 12:59:42 +0545

    Changs added to the SalesController

--- src/com/phar/controller/SalesController.java
+++ src/com/phar/controller/SalesController.java
@@ -1,11 +1,12 @@
 package com.phar.controller;
 
 import com.phar.billing;
+import com.phar.custom.CustomAlert;
 import com.phar.database.DatabaseConnection;
-import com.phar.extraFunctionality.AutoGenerator;
 import com.phar.extraFunctionality.CFunctions;
-import com.phar.extraFunctionality.DatabaseOperations;
+import com.phar.model.ProductEntry;
 import com.phar.model.Sales;
+import com.phar.model.SalesInfo;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -27,13 +28,28 @@ import java.util.ResourceBundle;
 /**
  * Created by Sam on 11/18/2016.
  */
+
 public class SalesController implements Initializable {
 
     int counter = 0;
     @FXML
-    private TextField pExpire, pAmount, pDiscount, pmrp, qEntered, qLeftInStore, pBatch, p_id1, total, pPrescribedBy, pAddress, pBillNo, pParty;
+    private TextField p_id1;
     @FXML
     private ComboBox<String> pName;
+    @FXML
+    private TextField pBatch;
+    @FXML
+    private TextField qLeftInStore;
+    @FXML
+    private TextField qEntered;
+    @FXML
+    private TextField pmrp;
+    @FXML
+    private TextField pDiscount;
+    @FXML
+    private TextField pAmount;
+    @FXML
+    private TextField pExpire;
     @FXML
     private TableColumn<Sales, String> proName;
     @FXML
@@ -53,8 +69,17 @@ public class SalesController implements Initializable {
     @FXML
     private TableColumn<Sales, Double> proAmount;
     @FXML
+    private TextField total;
+    @FXML
     private DatePicker tDate;
-
+    @FXML
+    private TextField pPrescribedBy;
+    @FXML
+    private TextField pAddress;
+    @FXML
+    private TextField pBillNo;
+    @FXML
+    private TextField pParty;
     private Connection connection;
     private PreparedStatement preparedStatement;
     private ResultSet resultSet;
@@ -63,16 +88,13 @@ public class SalesController implements Initializable {
     private List<Sales> productBill = new ArrayList<>();
     private String pIdd;
     private ObservableList<Sales> productTableList = FXCollections.observableArrayList();
+    private ObservableList<SalesInfo> salesInfoList = FXCollections.observableArrayList();
     private Double qtyLeft;
     private float newTotal = 0;
-    private String query;
-    private AutoGenerator generator = new AutoGenerator();
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-        pBillNo.setText(generator.CurrentID("BID"));
         tDate.setValue(LocalDate.now());
         try {
             connection = DatabaseConnection.getConnection();
@@ -118,6 +140,7 @@ public class SalesController implements Initializable {
         s.setExpireDate(pExpire.getText());
         s.setDiscount(Double.valueOf(pDiscount.getText()));
         s.setAmount(Double.valueOf(pAmount.getText()));
+
         s.setBillNo(pBillNo.getText());
         s.setParty(pParty.getText());
         s.setAddress(pAddress.getText());
@@ -125,6 +148,7 @@ public class SalesController implements Initializable {
         s.setProductDate(tDate.getValue().toString());
 
         productTableList.add(s);
+
         productBill.add(s);
 
         proId.setCellValueFactory(new PropertyValueFactory<Sales, String>("productID"));
@@ -144,20 +168,18 @@ public class SalesController implements Initializable {
         total.setText(String.valueOf(newTotal));
 
         Double newQty = qtyLeft - Double.valueOf(qEntered.getText());
-        query = "UPDATE store SET quantity='" + newQty + "' WHERE product_name = '" + pName.getValue() + "'";
-        System.out.println(query);
+        String sql1 = "UPDATE store SET quantity='" + newQty + "' WHERE product_name = '" + pName.getValue() + "'";
+        System.out.println(sql1);
         try {
-//            preparedStatement = connection.prepareStatement(query);
-//            preparedStatement.executeUpdate();
-            DatabaseOperations.simpleUpdate(query);
-//            query = "SELECT quantity FROM store WHERE product_name ='" + pName.getValue() + "'";
-//            preparedStatement = connection.prepareStatement(query);
-//            resultSet = preparedStatement.executeQuery();
-            resultSet = DatabaseOperations.simpleSelect("store", "quantity", "product_name='" + pName.getValue() + "'");
+            preparedStatement = connection.prepareStatement(sql1);
+            preparedStatement.executeUpdate();
+            sql1 = "SELECT quantity FROM store WHERE product_name ='" + pName.getValue() + "'";
+            preparedStatement = connection.prepareStatement(sql1);
+            resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 qLeftInStore.setText(resultSet.getString("quantity"));
             }
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
@@ -165,5 +187,40 @@ public class SalesController implements Initializable {
     @FXML
     void onClickPrintBill(ActionEvent event) {
         billing.bill(productBill, counter);
+    }
+
+    public void onClickSaveButton(ActionEvent actionEvent) {
+        SalesInfo si = new SalesInfo();
+        si.setSalesBill(pBillNo.getText());
+        si.setSalesParty(pParty.getText());
+        si.setCustomerAddress(pAddress.getText());
+        si.setPrescribedBy(pPrescribedBy.getText());
+        si.setProductName(pName.getValue());
+        si.setSalesDate(tDate.getValue().toString());
+        si.setProductQuantity(Integer.valueOf(qEntered.getText()));
+        si.setSalesAmount(Float.valueOf(pAmount.getText()));
+
+        salesInfoList.add(si);
+
+        String query = "INSERT into sales_info(sales_bill, sales_party, customer_address, prescribed_by, product_name, sales_date, sales_amount, product_quantity) VALUES (?,?,?,?,?,?,?,?) ";
+        for (SalesInfo salesInfo : salesInfoList) {
+            try{
+                preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, salesInfo.getSalesBill());
+                preparedStatement.setString(2, salesInfo.getSalesParty());
+                preparedStatement.setString(3, salesInfo.getCustomerAddress());
+                preparedStatement.setString(4, salesInfo.getPrescribedBy());
+                preparedStatement.setString(5, salesInfo.getProductName());
+                preparedStatement.setString(6, salesInfo.getSalesDate());
+                preparedStatement.setFloat(7, salesInfo.getSalesAmount());
+                preparedStatement.setInt(8, salesInfo.getProductQuantity());
+                preparedStatement.executeUpdate();
+                }
+            catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+        CustomAlert alert = new CustomAlert("Info.", "Save Successful");
+        alert.withoutHeader();
     }
 }
